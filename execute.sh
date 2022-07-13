a6c35f8deb7b56c0cafeaf683b95217400e310f2
orvpat <109121988+orvpat@users.noreply.github.com>
2022-07-13 12:59:43 -0400

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,1 +1,1 @@
-imagenet-camera --model=/shapes_project/models/shapes/resnet18.onnx --input_blob=input_0 --output_blob=output_0 --labels=/shapes_project/data/shapes/labels.txt /dev/video0 --width=640 --height=480 rtp://192.168.5.102:1234
+shapes_run.py --model=/shapes_project/models/shapes/resnet18.onnx --input_blob=input_0 --output_blob=output_0 --labels=/shapes_project/data/shapes/labels.txt /dev/video0 --width=640 --height=480 rtp://192.168.5.102:1234
